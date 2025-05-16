36a7e8a2c425eff0eef4d7851ebe6f98918bbbc3
Vivar1 <jayden.vivar1@studytafensw.edu.au>
2025-05-16 10:48:10 +1000

    Added new nav bar

--- src/app/partials/navbar.tsx
+++ src/app/partials/navbar.tsx
@@ -54,7 +54,7 @@ const Navbar = () => {
                     }`}
                   >
                    
-                      Services
+                      Join us
                     
                   </Link>
                 </li>
